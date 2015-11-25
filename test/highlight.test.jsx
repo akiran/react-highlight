27bd5ad02068eefcbf515de9e53be9bac235f539
iamblue <tonyone0902@gmail.com>
2015-11-25 18:14:54 +0800

    fixed all test error

--- test/highlight.test.jsx
+++ test/highlight.test.jsx
@@ -1,36 +1,37 @@
 var Highlight = require('../');
-var TestUtils = React.addons.TestUtils;
-
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var ReactDOMServer = require('react-dom/server');
 describe('highlight', function() {
 
   it('should display test inside it', function() {
     var text = TestUtils.renderIntoDocument(
       <Highlight>Some text</Highlight>
     );
-    expect(text.getDOMNode().textContent).toBe('Some text');
+    expect(ReactDOM.findDOMNode(text).textContent).toBe('Some text');
   });
 
   it('should have pre and code tags in markup', function() {
-    var text = React.renderToStaticMarkup(
+    var text = ReactDOMServer.renderToStaticMarkup(
       <Highlight>Some text</Highlight>
     );
-    expect(text.replace(/\s/, '')).toBe('<pre><code>Some text</code></pre>');
+    expect(text).toBe('<pre><code>Some text</code></pre>');
   });
 
   it('should assign className prop', function() {
-    var text = React.renderToStaticMarkup(
+    var text = ReactDOMServer.renderToStaticMarkup(
       <Highlight className='html'>Some text</Highlight>
     );
-    expect(text.replace(/\s/, '')).toBe('<pre><code class="html">Some text</code></pre>');
+    expect(text).toBe('<pre><code class="html">Some text</code></pre>');
   });
 
   it('should accept innerHTML prop', function() {
     var text = TestUtils.renderIntoDocument(
-      <Highlight innerHTML={true}>{"<div>Some text</div>"}</Highlight>
+      <Highlight innerHTML={true}>{"<div>Sometext</div>"}</Highlight>
     );
-    expect(text.getDOMNode().textContent).toBe('Some text');
+    expect(ReactDOM.findDOMNode(text).textContent).toBe('Sometext');
   });
-  
+
 });
 
 
