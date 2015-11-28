daaaf5e7535593199167d986ccbac7baa3a869a4
kiran <kiran@webrafter.com>
2015-11-28 11:15:38 +0530

    Added some docs

--- docs/index.jsx
+++ docs/index.jsx
@@ -1,7 +1,25 @@
-var React = require('react');
-var Router = require('react-router');
-var routes = require('./routes');
+// var React = require('react');
+// var Router = require('react-router');
+// var routes = require('./routes');
 
-Router.run(routes, function (Handler) {
-  React.render(<Handler />, document.body);
-});
+// Router.run(routes, function (Handler) {
+//   React.render(<Handler />, document.body);
+// });
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Highlight = require('../src/index');
+
+var Docs = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <Highlight>Some text</Highlight>
+        <Highlight>{'<p>Some text</p>'}</Highlight>
+        <Highlight className='js'>{'var a, b, c;\n c = a + b;'}</Highlight>
+      </div>
+    );
+  }
+})
+
+ReactDOM.render(<Docs />, document.getElementById('docs'));
